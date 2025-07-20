d72e1b00a45b6b4427392f954cf3d1f1cb0a321f
justin-theodorus <justin.theodorus@gmail.com>
2025-07-20 13:39:19 +0800

    implement search feature

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,13 @@ const [movieList, setMovieList] = useState([]);
 const [isLoading, setIsLoading] = useState(false);
 
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
       if (!response.ok) {
         throw new Error('Network response was not ok');
@@ -50,8 +52,8 @@ const [isLoading, setIsLoading] = useState(false);
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, [])
+    fetchMovies(searchTerm);
+  }, [searchTerm])
   
   return (
     <main>
